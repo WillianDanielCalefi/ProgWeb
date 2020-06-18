78ccccf1887ffa997a3c6ccf2a0c87d367835944
joaolucas26 <JOAO.LUCAS26@OUTLOOK.COM>
2020-06-18 11:26:19 -0300

    O KRL DO ID N DA CERTOOOOOOO

--- edit_prod.php
+++ edit_prod.php
@@ -1,8 +1,8 @@
 <?php
 
 require_once('config/config.php');
-require ('menu.php');
-//session_start();
+//require ('menu.php');
+session_start();
 
     
     if($_SESSION['nivel'] > 1){
@@ -24,74 +24,80 @@ require ('menu.php');
             ";
     }
 
-    if(isset($_POST['cadastrar'])){  
-       //varivavel para verificar se é possivel seguir com o upload
-        $uploadOK = 1;       
-        // salvando diretorio para salvar as imagens
-        $target = "uploads/".basename($_FILES['image']['name']);
-        //para checar o tipo da imagem
-        $imageFileType = strtolower(pathinfo($target,PATHINFO_EXTENSION));
-        // salvando o tamanho da imagem
-        $check = getimagesize($_FILES["image"]["tmp_name"]);
-        
-          if($check !== false) {
-            $uploadOk = 1;
-            }else {
-            $uploadOk = 0;
+    if(isset($_POST['cadastrar'])){           
+        if (isset($_POST['nome']) && empty($_POST['nome']) == false){
+          $nome = addslashes($_POST['nome']);
+          $preco = addslashes($_POST['preco']);
+          $tipo = addslashes($_POST['tipo']);
+
+          //CASO SEJA REALIZADO O UPDATE DA IMAGEM DO PRODUTO, REFAZ A VERIFICAÇÃO
+          if (!isset($_FILES['image']['name'])){  
+                $uploadOK = 1;       
+                $target = "uploads/".basename($_FILES['image']['name']);
+                $imageFileType = strtolower(pathinfo($target,PATHINFO_EXTENSION));
+                $check = getimagesize($_FILES["image"]["tmp_name"]);
             
-          }
-        
-        // checar o tamanho da imagem
-        if ($_FILES["image"]["size"] > 500000) {
-          $uploadOk = 0;          
-        }
+                if($check !== false) {
+                  $uploadOk = 1;
+                  }else {
+                  $uploadOk = 0;                
+                  }
 
-        // checando se é uma imagem 
-        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
-          $uploadOk = 0;          
-        }
+                if ($_FILES["image"]["size"] > 500000) {
+                  $uploadOk = 0;          
+                }
 
-        if ($uploadOk == 0){          
-          echo "
-          <META HTTP-EQUIV=REFRESH CONTENT='0; url=insert_prod.php'>
-          <script type=\"text/javascript\">
-           alert(\"Faça upload da imagem do produto!\"); </script>
-          ";
-        }else{    
-              if (isset($_POST['nome']) && empty($_POST['nome']) == false){
-                $nome = addslashes($_POST['nome']); // varivel nome
-                $preco = addslashes($_POST['preco']);
-                $img = $_FILES['image']['name'];
-                $tipo = addslashes($_POST['tipo']);
+                if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+                  $uploadOk = 0;          
+                }
 
-                 
+                if ($uploadOk == 0){  
+                  echo "
+                  <META HTTP-EQUIV=REFRESH CONTENT='0; url=gerenciamento_prod.php'>
+                  <script type=\"text/javascript\">
+                  alert(\"Faça upload da imagem do produto!\"); </script>
+                  ";
 
                 
-                //Insere os registros no BD
-                $sql = "UPDATE produtos SET nome = '$nome', preco = '$preco', img = '$img', tipo = '$tipo'";      
-                $sql = $conn->query($sql); // executa o insert
+                 
+                }else{
+                    $img = $_FILES['image']['name'];          
+                    $sql = "UPDATE produtos SET nome = '$nome', preco = '$preco', img = '$img', tipo = '$tipo'";      
+                    $sql = $conn->query($sql); 
 
-                if (move_uploaded_file($_FILES["image"]["tmp_name"], $target)) {
-                  echo "
-                    <META HTTP-EQUIV=REFRESH CONTENT='0; url=gerenciamento_prod.php'>
-                    <script type=\"text/javascript\">
-                    alert(\"Upload do produto Concluido!\"); </script>
-                    ";
-                } else {
-                  echo "
-                    <META HTTP-EQUIV=REFRESH CONTENT='0; url=gerenciamento_prod.php'>
-                    <script type=\"text/javascript\">
-                    alert(\"Não foi possivel realizar o cadastro do produto!\"); </script>
-                    ";
-                }
-             }  
-          }
-
+                    if (move_uploaded_file($_FILES["image"]["tmp_name"], $target)) {
+                      echo "
+                        <META HTTP-EQUIV=REFRESH CONTENT='0; url=gerenciamento_prod.php'>
+                        <script type=\"text/javascript\">
+                        alert(\"Upload do produto Concluido!\"); </script>
+                        ";
+                    } else {
+                      echo "
+                        <META HTTP-EQUIV=REFRESH CONTENT='0; url=gerenciamento_prod.php'>
+                        <script type=\"text/javascript\">
+                        alert(\"Não foi possivel realizar o cadastro do produto!\"); </script>
+                        ";
+                    }
+              }
+        
+      }else{
+          //Insere os registros no BD
+          $sql = "UPDATE produtos SET nome = '$nome', preco = '$preco', tipo = '$tipo'";      
+          $sql = $conn->query($sql); // executa o insert
+               echo "
+              <META HTTP-EQUIV=REFRESH CONTENT='0; url=gerenciamento_prod.php'>
+              <script type=\"text/javascript\">
+              alert(\"Upload do produto Concluido!\"); </script>
+              ";
+          
+        } 
+      }
   }
+
 
     if (isset($_GET['id']) && empty($_GET['id']) == false){
     $id = addslashes($_GET['id']);
-    }
+     }
 
         //apresenta apenas os dados do "id" que foi selecionado para Editar
     $sql = "SELECT * FROM produtos WHERE id = '$id'";
@@ -126,7 +132,7 @@ require ('menu.php');
      <!------------------------INICIO DA TABELA DE CADASTRO----------------->
     <div class="container">
      <div id="cadastro" style="padding-top: 78px; position: center">
-        <form method="POST" action="insert_prod.php" enctype="multipart/form-data" > 
+        <form method="POST" action="edit_prod.php" enctype="multipart/form-data" > 
           <h1>Cadastro de Produtos</h1> 
            
           <p> 
@@ -141,7 +147,7 @@ require ('menu.php');
 
           <p> 
             <label for="image">Imagem do Produto</label>
-            <input id="image" name="image" type="file" value="<?php echo $dado['img']?>"> 
+            <input id="image" name="image" type="file"> 
           </p>
            
           <p> 
